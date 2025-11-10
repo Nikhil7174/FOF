1522a615dba928d612abf7114e2b496ee82d8615
nikhil7174 <nikhilkumarsingh7174@gmail.com>
2025-11-10 16:29:11 +0530

    cors header fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -94,8 +94,26 @@ app.use(cors({
     return callback(new Error("Not allowed by CORS"));
   },
   credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  exposedHeaders: ["Content-Type", "Authorization"],
   optionsSuccessStatus: 200,
+  preflightContinue: false,
 }));
+
+// Explicit OPTIONS handler for preflight requests
+app.options("*", (req, res) => {
+  const origin = req.headers.origin;
+  if (origin && isAllowedOrigin(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+    res.setHeader("Access-Control-Max-Age", "86400"); // 24 hours
+  }
+  res.sendStatus(200);
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
